459e1e3595cb8215917ac73d875d9cf8dbb1920c
Arian N. Yamchelo <ariannajafi@outlook.com>
2022-10-20 10:59:53 +1100

    Minor changes and fixes

--- music/auth.py
+++ music/auth.py
@@ -2,6 +2,13 @@ import boto3
 from flask import Blueprint, redirect, session, flash, render_template, url_for
 from music.models import Logins, LoginForm, RegisterForm
 
+# [21] "What is Python KeyError and How to Handle it with 3 Examples", A-Z Tech, 2022. [Online]. Available:
+# https://www.jquery-az.com/python-keyerror-handle-3-examples/. [Accessed: 19- Oct- 2022].
+#
+# [22] C. Hansen, "Python KeyError Exceptions and How to Handle Them – Real Python", Realpython.com, 2022. [Online].
+# Available: https://realpython.com/python-keyerror/. [Accessed: 19- Oct- 2022].
+
+
 auth = Blueprint('auth', __name__, template_folder="templates/music")
 
 logins = Logins(boto3.resource('dynamodb', 'ap-southeast-2'))
